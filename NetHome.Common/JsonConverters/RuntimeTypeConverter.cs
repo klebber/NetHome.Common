3f65c6fd5070ab2eea51d4e25216681f0a72e940
klebber <bstojev199@gmail.com>
2021-10-29 16:20:34 +0200

    Bugfix

--- NetHome.Common/JsonConverters/RuntimeTypeConverter.cs
+++ NetHome.Common/JsonConverters/RuntimeTypeConverter.cs
@@ -20,19 +20,25 @@ namespace NetHome.Common.JsonConverters
             Type typeToConvert,
             JsonSerializerOptions options)
         {
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException();
             if (!reader.Read() || reader.TokenType != JsonTokenType.PropertyName) 
                 throw new JsonException();
             string typeName = reader.GetString();
             string qualifiedName = $"{assembly1}{typeName}, {assembly2}";
             Type type = Type.GetType(qualifiedName, true);
-
-            return (T)JsonSerializer.Deserialize(ref reader, type); ;
+            T t = (T)JsonSerializer.Deserialize(ref reader, type, options);
+            if (!reader.Read() || reader.TokenType != JsonTokenType.EndObject)
+                throw new JsonException();
+            return t;
         }
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
         {
+            writer.WriteStartObject();
             writer.WritePropertyName(value.GetType().Name);
             JsonSerializer.Serialize(writer, value, value?.GetType() ?? typeof(object), options);
+            writer.WriteEndObject();
         }
     }
 }
