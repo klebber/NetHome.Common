81a5a4efdf26c477aab242d4d60f135ca13475da
klebber <bstojev199@gmail.com>
2022-06-15 23:13:25 +0200

    Add dto classes for password change and device access

---
+++ NetHome.Common/DeviceAccessPayload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetHome.Common
+{
+    public class DeviceAccessPayload
+    {
+        public int DeviceId { get; set; }
+        public string UserId { get; set; }
+    }
+}
