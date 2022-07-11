3172d67ae6e71f9cacd812891fbd033d0553a1da
klebber <bstojev199@gmail.com>
2022-07-11 22:53:35 +0200

    Add RoomModel and DeviceTypeModel

---
+++ NetHome.Common/DeviceTypeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetHome.Common
+{
+    public class DeviceTypeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ NetHome.Common/RoomModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetHome.Common
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
